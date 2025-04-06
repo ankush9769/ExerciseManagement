a0e5db5198ecb82b269bbdf18dd09749ea2586ce
Ankush pal <155830977+ankush9769@users.noreply.github.com>
2025-04-06 12:05:29 +0530

    exercise delete operation created

--- src/component/EditExercise.jsx
+++ src/component/EditExercise.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 
 const EditExercise = () => {
   return (
-    <div>
-        <h1>Edit Exercise</h1>  
-      
-    </div>
+    <div className='container mt-4'>
+            <form onSubmit={handleSubmit}>
+                <legend className="mb-3">Add your Exercise</legend>
+                <div className="mb-3">
+                    <label className="form-label">username</label>
+                    <input type="text" className="form-control" placeholder="username" value={username} onChange={onchangeUsername} />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">description</label>
+                    <input type="text" className="form-control" placeholder="description" value={description} onChange={onchangeDescription} />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">duration</label>
+                    <input type="Number" className="form-control" placeholder="duration" value={duration} onChange={onchangeDuration} />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">date</label>
+                    <input type="date" className="form-control" placeholder="date" value={date} onChange={onchangeDate} />
+                </div>
+                <button type="submit" className="btn btn-primary" >Create Exercise</button>
+            </form>
+        </div>
   )
 }
 
--- src/component/ExerciseList.jsx
+++ src/component/ExerciseList.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 const ExerciseList = () => {
+  const [exercises, setExercises] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/exercise')
+      .then(response => {
+        setExercises(response.data);
+        // console.log(response.data)
+      })
+      .catch(error => console.error("there is something problem to retrive the list of Exercise" + error))
+  }, []);
+
+  const ondeletehandle = (id) => {             //this for delelte the execise from the list of the exercise
+    axios.delete(`http://localhost:5000/exercise/${id}`)
+      .then(response => {
+        console.log("exercise deleted" + response.data)
+        setExercises(prevExercises => prevExercises.filter(ex => ex._id !== id));    //to remove the delelted exercise from the list of the exercise(state)
+      })
+      .catch(error => console.error("there is something problem to delete the exercise" + error))
+  }
+
+
   return (
-    <div>
-        <h1>Exercise List</h1>
-      
+    <div className='container'>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">SR.</th>
+            <th scope="col">Username</th>
+            <th scope="col">description</th>
+            <th scope="col">duration</th>
+            <th scope="col">date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {exercises.map((exercise, index) => (
+            <tr key={exercise._id}>
+              <th scope="row">{index + 1}</th>
+              <td>{exercise.username}</td>
+              <td>{exercise.description}</td>
+              <td>{exercise.duration}</td>
+              <td>{exercise.date}</td>
+              <td>
+                <Link to={`/edit/${exercise._id}`} className="btn btn-outline-primary ">edit</Link>
+                <button className="btn btn-outline-danger ms-3" onClick={()=>ondeletehandle(exercise._id)}>delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
     </div>
   )
 }
